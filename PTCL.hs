3593f4bbca14674064099da5e3d8a7d890976a6f
Ghadeer Alkubaish <ghadeeralkubaish@gmail.com>
2018-04-18 22:39:32 -0700

    first haskel implementation

--- PTCL.hs
+++ PTCL.hs
@@ -9,50 +9,57 @@ type Domain = (Prolog, TypeSpec) -> Report
 
 -- | The output of PTCL is True or False.
 --   Should have more detail error message in the future
-type Report = Bool
+
+type Error = String
+type Report = (Bool, Error)
 
 -- | A set of Prolog Predicate 
-type Prolog = Undefined 
+type Prolog = [Rules] 
 
 -- | A list of the type declaration 
 type TypeSpec = [Decl]
-
 -- 
 -- * Basic Object of Type Declaration
 -- 
 
 -- | Type Declaration reoresented by:
 --   The Name of the predicate associated with type of its arguments
-type Decl = (Name, [Type])
+type Decl = (PredName, [Type])
 
 -- | Name is the name of the predicate 
-type Name = String 
+type PredName = String -- lower case 
 -- (Shouldn't be the String here, but don't know what it should be )
 
+
 -- | Basic Type in Prolog 
-data Type = TAtom | TInt | TList
-
-
-
+data Type = TAtom Object| TInt Int| TString String| TList [Type]
 --
 -- * Basic Object of Prolog Program 
 --
 
+data Object = Mona | Jacki
+
 -- | Predicate in Prolog, Name + Arguments
-data Predicate = Pred Name [Argu] -- at least one element 
+data PredicateT = Pred PredName [Type] -- at least one element
 
-type Argu = Predicate 
-          | Atom  
-          | Expr
+type VarName = String -- upper case 
 
-data Atom = Undefined
+data Opt = Eq | Neq | And | Lt | Leq | Gt | Gtq | Sub | Add | Div | Mult| Mod 
+ 
+data BodyElem  = Predicate PredicateT
+     | Bind VarName BodyElem  
+     | Oper Opt BodyElem BodyElem 
+     | Lit Int 
+     | Ref VarName
 
-data Expr = Undefined
+type Body = [BodyElem]
 
 -- | Rules in Prolog, Head + Body 
-data Rules = Head Predicate [Predicate]
+data Rules = Head PredicateT Body
 
 
+-- femalMona:: PredicateT
+-- femalMona = (Pred "female" [TAtom Mona])
 
 
 
