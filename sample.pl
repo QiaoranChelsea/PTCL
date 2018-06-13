f286e26e11e2b7accf7a9d90a32d3317aa4f8d38
Ghadeer Alkubaish <ghadeeralkubaish@gmail.com>
2018-06-12 21:34:00 -0700

    done with code hopefully

--- sample.pl
+++ sample.pl
@@ -1,7 +1,7 @@
-data tree _a = node(_a,tree _a ,tree _a ) | lea.
+data tree _a = node(_a,tree _a ,tree _a ) | leaf.
 type myList = list.
 data tree = node(string,tree ,tree ) | leaf.
-data treeAB _a _b = node(_a,treeAB _a _b,treeAB _a _b) | leaf(_b).
+data treeAB _a _b = nodeAB(_a,treeAB _a _b,treeAB _a _b) | leafAB(_b).
 
 
 decl female(atom).
@@ -9,7 +9,7 @@ decl female(string).
 decl age(atom,int).
 decl married_(atom, atom).
 decl married(atom, atom).
-decl tree(tree _a ).
+decl myTree(tree _a ).
 decl isTree(tree _a).
 decl sumTree(tree _a , int).
 decl listLength(myLi, int).
@@ -31,7 +31,7 @@ age(mona, 6).
 age(jacki, 19, 19).
 age(20,marge).
 
-doubleAge(A,T):-  age(Y,A) , T is A *2.
+doubleAge(A,T):-  age(Y,A) , T is A * 2.
 
 married_(abe,mona).
 married_(clancy,jackie).
@@ -51,9 +51,9 @@ comTwo(X,Y) :- X = 5 , age(X,Y).
 
 double(X, Y) :- X * 2 is Y.
 
-tree(leaf).
-tree(node(leaf , leaf, leaf)).
-tree(node(4, leaf, node(3,leaf(4), node(4,leaf, leaf)))).
+myTree(leaf).
+myTree(node(leaf , leaf, leaf)).
+myTree(node(4, leaf, node(3,leaf(4), node(4,leaf, leaf)))).
 
 isTree(leaf).
 isTree(node(_, L, R)):- isTree(L),isTree(R).
@@ -62,8 +62,8 @@ sumTree(0, leaf).
 sumTree(node(I, L, R), T ):- sumTree(L, N1), sumTree(R, L ), T is N1 + I.
 sumTree(node(I, L, R), T ):- sumTree(L, N1), sumTree(R, N2), T is N1 + R + I.
 
-formTree(X,Y, nodeAB(X,leafAB(Y),leafAB(Y))) :- X = 5 , Y = "S", X = Y.
-formTree(X,Y, nodeAB(X,leafAB(Y),leafAB(Y))) :- X = 5 , Y = "S".
+formTree(X,Y, nodeAB(X,leafAB(Y),leafAB(Y))) :- X = 5 , Y = "S", X is Y.
+formTree(X,Y, nodeAB(X,Y,leafAB(Y))) :- X = 5 , Y = "S".
 
 listLength([], 0).
 listLength([_|T], Total):-  listLength(T, N) , Total is 1 + N.
